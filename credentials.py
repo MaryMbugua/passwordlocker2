ed1a95ab4d667d9faf8187e12561c29f4047f9b8
Mary Mbugua <marymbugua.nm@gmail.com>
2018-04-02 20:31:57 +0300

    created class credentials

---
+++ credentials.py
@@ -0,0 +1,11 @@
+import string
+import secrets
+import random
+import pyperclip
+
+class Credentials:
+    '''
+    CLASS THAT GENERATES A NEW INSTANCE OF A USER/
+    CREATES A NEW ACCOUNT
+    '''
+    credential_list = []
