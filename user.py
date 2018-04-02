670c8ea567e6909618325fd6ed46f30fb0fe0d5d
Mary Mbugua <marymbugua.nm@gmail.com>
2018-04-02 20:24:49 +0300

    test to save multiple users

---
+++ user.py
@@ -0,0 +1,23 @@
+import string
+import secrets
+import random
+import pyperclip
+
+class User:
+    '''
+    CLASS THAT GENERATES A NEW INSTANCE OF A USER/
+    CREATES A NEW ACCOUNT
+    '''
+    user_list = []
+    def __init__(self,first_name,last_name,user_name,phone_number,email,password):
+        '''
+        initialises a new instance of a credential
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.email = email
+        self.phone_number = phone_number
+        self.password= password
+    def save_user(self):
+        User.user_list.append(self)
--- user_test.py
+++ user_test.py
@@ -26,8 +26,17 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.password,"lovepy") 
     def test_save_user(self):
         '''
-        to test if user object is saved
+         to test if user object is saved
          into user list
         '''
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
+    def test_save_multipleuser(self):
+        '''
+         to test if we can save multiple users
+         to the user list
+        '''
+        self.new_user.save_user()
+        test_user = User("testfirst","testlast","testusername","0712345678","test@user.com","testpassword")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
