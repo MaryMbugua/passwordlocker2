cfc4ba21519bd5a4c9458a072ea6ff2acfa7653e
Mary Mbugua <marymbugua.nm@gmail.com>
2018-04-02 20:50:59 +0300

    test to run before each test case and cleanup

--- credentials_test.py
+++ credentials_test.py
@@ -2,3 +2,15 @@ import unittest ##import unittest module
 from credentials import Credentials ##import credential class from credentials.py file
 import pyperclip
 
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        '''
+        method to run before each test case
+        '''
+        self.new_credential = Credentials("facebook","berylmmbone","mypass") #create user object
+    def tearDown(self):
+        '''
+        cleans up after each test case has 
+        been executed
+        '''
+        Credentials.credential_list = []
