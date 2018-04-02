a661bb9a8f479fde13ca01be04775d1a60115860
Mary Mbugua <marymbugua.nm@gmail.com>
2018-04-02 20:55:17 +0300

    methd to initialise a new credential and to save a new credential

--- credentials.py
+++ credentials.py
@@ -9,3 +9,12 @@ class Credentials:
     CREATES A NEW ACCOUNT
     '''
     credential_list = []
+    def __init__(self,app_name,account_name,account_password):
+        '''
+        initialises a new instance of a credential
+        '''
+        self.app_name = app_name
+        self.account_name = account_name
+        self.account_password= account_password
+    def save_newcredential(self):
+        Credentials.credential_list.append(self)
