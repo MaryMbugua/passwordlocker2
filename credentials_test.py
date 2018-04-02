87e3e7ba52a06c81839105cfaf68b1df2fd730e6
Mary Mbugua <marymbugua.nm@gmail.com>
2018-04-02 20:53:28 +0300

    test to initialise and save a credential

--- credentials_test.py
+++ credentials_test.py
@@ -14,3 +14,17 @@ class TestUser(unittest.TestCase):
         been executed
         '''
         Credentials.credential_list = []
+    def test_init(self):
+        '''
+        to test if object is initialised properly
+        '''
+        self.assertEqual(self.new_credential.app_name,"facebook")
+        self.assertEqual(self.new_credential.account_name,"berylmmbone")
+        self.assertEqual(self.new_credential.account_password,"mypass")
+    def test_save_newcredential(self):
+        '''
+        to test if credential object is saved
+         into credential list
+        '''
+        self.new_credential.save_newcredential()
+        self.assertEqual(len(Credentials.credential_list),1)
